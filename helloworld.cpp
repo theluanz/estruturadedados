da8400627e1ffe51d2f6bdeacb0f4e927633c4af
theluanz <luanzuffo.lz@gmail.com>
2020-08-04 20:54:59 -0300

    Classe Hello World

---
+++ helloworld.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int num1, num2;
+    cout << "Primeiro numero: ";
+    cin >> num1;
+    cout << "Segundo numero: ";
+    cin >> num2;
+    cout << "A soma é = " << num1 + num2 << endl;
+}
